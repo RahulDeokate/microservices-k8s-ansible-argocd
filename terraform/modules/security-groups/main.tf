e403445f6afb762a854e286d83a2afd64e306ae2
Rahul Deokate <rahuldeokate47666@gmail.com>
2025-08-29 08:15:51 +0530

    separated frontend and backend into their own base folders

--- terraform/modules/security-groups/main.tf
+++ terraform/modules/security-groups/main.tf
@@ -59,3 +59,8 @@ resource "aws_iam_role_policy_attachment" "eks_node_AmazonEKS_CNI_Policy" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
